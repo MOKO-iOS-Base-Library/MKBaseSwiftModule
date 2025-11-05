a52adc4c4ef62c5bf15f0ca550a30e86b713e417
lovexiaoxia <aadyx2007@163.com>
2025-11-05 18:06:01 +0800

    优化基础组件库，去掉所有UI相关的

--- Package.swift
+++ Package.swift
@@ -1,10 +1,10 @@
-// swift-tools-version: 6.0
+// swift-tools-version: 6.1
 import PackageDescription
 
 let package = Package(
     name: "MKBaseSwiftModule",
     platforms: [
-        .iOS(.v14),
+        .iOS(.v15),
     ],
     products: [
         .library(
@@ -12,31 +12,13 @@ let package = Package(
             targets: ["MKBaseSwiftModule"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/devicekit/DeviceKit.git", .upToNextMajor(from: "5.0.0")),
-        .package(url: "https://github.com/WenchaoD/FSCalendar.git", .upToNextMajor(from: "2.8.4")),
-        .package(url: "https://github.com/hackiftekhar/IQKeyboardManager.git", .upToNextMajor(from: "6.5.0")),
-        .package(url: "https://github.com/CoderMJLee/MJRefresh.git", .upToNextMajor(from: "3.7.6")),
-        .package(url: "https://github.com/SnapKit/SnapKit.git", .upToNextMajor(from: "5.6.0")),
-        .package(url: "https://github.com/SwiftyJSON/SwiftyJSON.git", .upToNextMajor(from: "5.0.0")),
-        .package(url: "https://github.com/raulriera/TextFieldEffects.git", .upToNextMajor(from: "1.3.0")),
-        .package(url: "https://github.com/scalessec/Toast-Swift.git", .upToNextMajor(from: "5.0.0")),
-        .package(url: "https://github.com/weichsel/ZIPFoundation.git", from: "0.9.16"),
-        .package(url: "https://github.com/jmcnamara/libxlsxwriter", from: "1.2.3"),
+        
     ],
     targets: [
         .target(
             name: "MKBaseSwiftModule",
             dependencies: [
-                .product(name: "DeviceKit", package: "DeviceKit"),
-                .product(name: "FSCalendar", package: "FSCalendar"),
-                .product(name: "IQKeyboardManagerSwift", package: "IQKeyboardManager"),
-                .product(name: "MJRefresh", package: "MJRefresh"),
-                .product(name: "SnapKit", package: "SnapKit"),
-                .product(name: "SwiftyJSON", package: "SwiftyJSON"),
-                .product(name: "TextFieldEffects", package: "TextFieldEffects"),
-                .product(name: "Toast", package: "Toast-Swift"),
-                .product(name: "ZIPFoundation", package: "ZIPFoundation"),
-                .product(name: "libxlsxwriter", package: "libxlsxwriter")
+                
             ],
             path: "Sources",
             resources: [
@@ -44,7 +26,7 @@ let package = Package(
             ],
             swiftSettings: [
                 .define("DEBUG", .when(configuration: .debug)),
-                .define("IOS14_OR_LATER")
+                .define("IOS15_OR_LATER")
             ],
             linkerSettings: [
                 .linkedLibrary("z"),
